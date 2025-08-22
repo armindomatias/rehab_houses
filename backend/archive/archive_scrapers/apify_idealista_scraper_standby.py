0498e4ba1a558fc8350be0ea231441a4130b938c
Armindo Matias <armindomatiasg@gmail.com>
2025-08-23 00:11:35 +0100

    changesss

--- backend/archive/archive_scrapers/apify_idealista_scraper_standby.py
+++ backend/archive/archive_scrapers/apify_idealista_scraper_standby.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import time
+import os
 
 # Standby mode actor ("Faster")
 
@@ -11,13 +12,13 @@ the Actor remains ready in the background to handle arbitrary HTTP requests,
 just like any web or API server.
 """
 
-APIFY_USER_ID = 0 #get it from env
-APIFY_API_TOKEN = 0 #get it from env
-ACTOR_ID = 0 #get it from env
+APIFY_USER_ID = 0 # get it from env
+APIFY_API_TOKEN = 0 # get it from env
+ACTOR_ID = 0 # get it from env
 
 url_apify = "https://api.apify.com/v2/acts/"
 
-url_actor = "https://dz-omar--idealista-scraper-api.apify.actor/"
+url_actor = 0 # get it from env
 
 url_listing = "https://www.idealista.pt/imovel/33970891/"
 
